bdb75eabe03c7b7a2ff420fb837437da39664af7
Ravelfett <ravelfett@gmail.com>
2023-09-15 11:28:52 +0200

    quadtree update

--- main.js
+++ main.js
@@ -35,20 +35,75 @@ function dotProduct(x1, y1, x2, y2) {
 }
 
 
+class Quadtree {
+  constructor(pos, size){
+    this.pos = pos.clone();
+    this.size = size.clone();
+    this.objs = [];
+    this.grid = [];
+    this.opened = false;
+  }
+  add(obj) {
+    if (this.objs.length <= 1){
+      this.objs.push(obj);
+      let s = this.size.mult(0.5);
+      this.grid = [
+        new Quadtree(this.pos, s),
+        new Quadtree(this.pos.add(new Vector(s.x, 0)), s),
+        new Quadtree(this.pos.add(new Vector(0, s.y)), s),
+        new Quadtree(this.pos.add(new Vector(s.x, s.y)), s),
+      ]
+    } else {
+      this.opened = true;
+      let s = this.size.mult(0.5);
+      if(obj.pos.x - this.pos.x < s.x){
+        if(obj.pos.y - this.pos.y < s.y) this.grid[0].add(obj)
+        else this.grid[2].add(obj)
+      }else{
+        if(obj.pos.y - this.pos.y < s.y) this.grid[1].add(obj)
+        else this.grid[3].add(obj)
+      }
+    }
+  }
+  getObjsIn(min, max){
+    if(!(min.x > this.pos.x + this.size.x ||
+      max.x < this.pos.x ||
+      min.y > this.pos.y + this.size.y ||
+      max.y < this.pos.y)){
+      
+    }
+  }
+  render(ctx){
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 2;
+    ctx.strokeRect(this.pos.x, this.pos.y, this.size.x, this.size.y);
+    for(let i in this.grid){
+      this.grid[i].render(ctx);
+    }
+  }
+}
+
 class World{
   constructor(){
     this.objects = [];
+    this.quadtree = new Quadtree(new Vector(0, 0), new Vector(width, height));
   }
   render(ctx){
     for(let i in this.objects){
       this.objects[i].render(ctx);
     }
+    this.quadtree.render(ctx);
   }
   update(){
     for(let i in this.objects){
       this.objects[i].update();
     }
-    for(let i in this.objects){
+    /*for(let obj of this.objects){
+      let objs = this.quadtree.getObjsIn(obj.pos.add(-1, -1), obj.pos.add(1, 1));
+      
+    }*/
+
+    /*for(let i in this.objects){
       for(let j in this.objects){
         if(i != j){
           let x1 = this.objects[i].pos.x, x2 = this.objects[j].pos.x;
@@ -66,7 +121,7 @@ class World{
           }
         }
       }
-    }
+    }*/
   }
 }
 
@@ -74,6 +129,9 @@ class Vector{
   constructor(x, y){
     this.x = x;
     this.y = y;
+  }
+  clone(){
+    return new Vector(this.x, this.y);
   }
   add(v){
     return new Vector(this.x+v.x, this.y+v.y);
@@ -112,12 +170,14 @@ class Object{
 }
 
 let wrld = new World();
-for(let i = 0; i < 50; i++){
-  let obj = new Object(Math.random()*width, Math.random()*height, 15)
+for(let i = 0; i < 500; i++){
+  //let obj = new Object(Math.random()*width, Math.random()*height, 5)
+  let obj = new Object(60, 60, 2);
   let angl = Math.random();
   obj.vel.x = Math.cos(angl*2*Math.PI);
   obj.vel.y = Math.sin(angl*2*Math.PI);
   wrld.objects.push(obj)
+  wrld.quadtree.add(obj);
 }
 
 function render(){
@@ -129,12 +189,15 @@ function render(){
   context.fill();
 
   wrld.update();
+  wrld.quadtree = new Quadtree(new Vector(0, 0), new Vector(width, height));
+  for(let obj of wrld.objects){
+    wrld.quadtree.add(obj);
+  }
+
+
   wrld.render(context);
 
 
   window.requestAnimationFrame(render);
 }
 window.requestAnimationFrame(render);
-
-
-
